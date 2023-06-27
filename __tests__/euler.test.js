67aa1f57b56d1b1c622b0f18dbd9df6f4ac85d5a
Aidan Williams <aidanwilliams01@gmail.com>
2023-06-27 15:35:09 -0700

    add test

--- __tests__/euler.test.js
+++ __tests__/euler.test.js
@@ -5,4 +5,8 @@ describe('multiples', () => {
   test('it returns the sum of the multiples of 3 or 5 below 10', () => {
     expect(determineTriangle()).toEqual(23);
   });
+
+  test('it returns the sum of the multiples of 3 or 5 below a given argument', () => {
+    expect(determineTriangle(1000)).toEqual(233168);
+  });
 });
