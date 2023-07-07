b6562cf3bb9ca996b9e8fc7657d67df76eb06672
Aidan Williams <aidanwilliams01@gmail.com>
2023-07-07 16:50:02 -0700

    update fibonacci code

--- src/euler.js
+++ src/euler.js
@@ -12,6 +12,22 @@ export function multiples(sumTo) {
 }
 
 export function fibonacci(sumTo) {
+  let array = [1, 2];
+  for (let index = 0; index < sumTo; index++) {
+    const element = array[index];
+    const element2 = array[index + 1];
+    const newElement = element + element2;
+    array.push(newElement);
+    if (newElement > sumTo) {
+      break;
+    }
+  }
   let sum = 0;
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if (element % 2 === 0) {
+      sum += element;
+    }
+  }
   return sum;
 }
